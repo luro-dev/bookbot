33b575bb820e0e550ec0d08a9fd26d4579311c9c
Luis Rodriguez <106412395+luro-dev@users.noreply.github.com>
2025-08-09 16:39:51 -0400

    Finish func to read book, func to get word count

---
+++ main.py
@@ -0,0 +1,14 @@
+from stats import get_num_words_in_book
+
+def get_book_text(filepath):
+    file_contents = None
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    frankenstein_text = get_book_text("books/frankenstein.txt")
+    frankenstein_text_word_count = get_num_words_in_book(frankenstein_text)
+    print(f"{frankenstein_text_word_count} words found in the document")
+
+main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+
+def get_num_words_in_book(book_words):
+    num_words = len(book_words.split())
+    return num_words 
+
