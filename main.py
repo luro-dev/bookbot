1793161191060d55e81b92117b9aa648cfd89276
Luis Rodriguez <106412395+luro-dev@users.noreply.github.com>
2025-08-09 16:47:35 -0400

    Finish character count func

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words_in_book
+from stats import get_num_words_in_book, get_num_characters_in_book
 
 def get_book_text(filepath):
     file_contents = None
@@ -10,5 +10,7 @@ def main():
     frankenstein_text = get_book_text("books/frankenstein.txt")
     frankenstein_text_word_count = get_num_words_in_book(frankenstein_text)
     print(f"{frankenstein_text_word_count} words found in the document")
-
+    
+    character_counts = get_num_characters_in_book(frankenstein_text)
+    print(character_counts)
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,15 @@
-
 def get_num_words_in_book(book_words):
     num_words = len(book_words.split())
     return num_words 
 
+def get_num_characters_in_book(book_words):
+    # includes symbols and spaces
+    character_count = {}
+
+    for character in book_words:
+        character = character.lower()
+        if character not in character_count:
+            character_count[character] = 1
+        else:
+            character_count[character] += 1
+    return character_count
